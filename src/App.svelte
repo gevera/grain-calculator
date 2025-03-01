f94513bac38c5b9db0d17ed4a61b922af138a815
“gevera” <skryonline@gmail.com>
2025-03-01 19:53:34 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li><strong>Acme Corp</strong></li>
+    </ul>
+    <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Products</a></li>
+    </ul>
+</nav>
+<section class="container">
+    <h1>Test</h1>
+</section>
